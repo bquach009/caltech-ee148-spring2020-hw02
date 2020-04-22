6e2e9bcb9218f645e71544d86e95635e0db260d2
Brandon Quach <bquach@caltech.edu>
2020-04-21 20:07:46 -1000

    Generate split completed

--- generate_split.py
+++ generate_split.py
@@ -1,14 +1,16 @@
 import numpy as np
 import os
+import json
 
 np.random.seed(2020) # to ensure you always get the same train/test split
 
-data_path = '../data/RedLights2011_Medium'
-gts_path = '../data/hw02_annotations'
-split_path = '../data/hw02_splits'
-os.makedirs(preds_path, exist_ok=True) # create directory if needed
+data_path = './data/RedLights2011_Medium'
+gts_path = './data/hw02_annotations'
+split_path = './data/hw02_splits'
+os.makedirs(split_path, exist_ok=True) # create directory if needed
+os.makedirs(gts_path, exist_ok=True)
 
-split_test = False # set to True and run when annotations are available
+split_test = True # set to True and run when annotations are available
 
 train_frac = 0.85
 
@@ -22,8 +24,13 @@ file_names = [f for f in file_names if '.jpg' in f]
 file_names_train = []
 file_names_test = []
 '''
-Your code below. 
+Your code below.
 '''
+
+file_names = np.random.permutation(file_names)
+split_idx = round(0.15*len(file_names))
+file_names_test = file_names[:split_idx]
+file_names_train = file_names[split_idx:]
 
 assert (len(file_names_train) + len(file_names_test)) == len(file_names)
 assert len(np.intersect1d(file_names_train,file_names_test)) == 0
@@ -32,21 +39,23 @@ np.save(os.path.join(split_path,'file_names_train.npy'),file_names_train)
 np.save(os.path.join(split_path,'file_names_test.npy'),file_names_test)
 
 if split_test:
-    with open(os.path.join(gts_path, 'annotations.json'),'r') as f:
+    with open(os.path.join(gts_path, 'formatted_annotations_mturk.json'),'r') as f:
         gts = json.load(f)
-    
+
     # Use file_names_train and file_names_test to apply the split to the
     # annotations
     gts_train = {}
     gts_test = {}
     '''
-    Your code below. 
+    Your code below.
     '''
-    
+    for item in gts:
+        if item in file_names_train:
+            gts_train[item] = gts[item]
+        else:
+            gts_test[item] = gts[item]
     with open(os.path.join(gts_path, 'annotations_train.json'),'w') as f:
         json.dump(gts_train,f)
-    
+
     with open(os.path.join(gts_path, 'annotations_test.json'),'w') as f:
         json.dump(gts_test,f)
-    
-    
